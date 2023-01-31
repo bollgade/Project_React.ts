e572b803cbb48efbb77043055e0fa9185e7eccf3
bollgade <bollgade@yandex.ru>
2023-01-31 07:58:59 +0300

    add TS to project

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const webpack = require('webpack');
 
 module.exports = {
 	mode: "development",
-	entry: path.resolve(__dirname, 'src', 'index.js'),
+	entry: path.resolve(__dirname, 'src', 'index.ts'),
 	output: {
 		filename: "[name].[contenthash].js",
 		path: path.resolve(__dirname, 'build'),
@@ -14,4 +14,16 @@ module.exports = {
 		new webpack.ProgressPlugin(),
 		new HTMLWebpackPlugin({ template: path.resolve(__dirname, 'public', 'index.html') }),
 	],
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				use: 'ts-loader',
+				exclude: /node_modules/,
+			},
+		],
+	},
+	resolve: {
+		extensions: ['.tsx', '.ts', '.js'],
+	},
 }
