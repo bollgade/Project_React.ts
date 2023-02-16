b1f806fe34a94fe7eec35f632de7411c1f07084e
bollgade <Bollgade@yandex.ru>
2023-02-16 13:37:46 +0300

    add icons for ThemeSwitcher

--- src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -1,8 +1,11 @@
 
-import { useTheme } from 'app/providers/ThemeProvider';
+import { Theme, useTheme } from 'app/providers/ThemeProvider';
 import { FC } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
-import cls from './ThemeSwitcher.module.scss'
+import cls from './ThemeSwitcher.module.scss';
+import LightIcon from 'shared/assets/icons/theme-light.svg';
+import DarkIcon from 'shared/assets/icons/theme-dark.svg';
+import User from 'shared/assets/icons/user-32-32.png';
 
 interface ThemeSwitcherProps {
 	className?: string;
@@ -11,14 +14,14 @@ interface ThemeSwitcherProps {
 export const ThemeSwitcher: FC<ThemeSwitcherProps> = (props) => {
 	const { className } = props;
 
-	const { toggleTheme } = useTheme();
+	const { theme, toggleTheme } = useTheme();
 
 	return (
 		<button
 			className={classNames(cls.themeSwitcher, {}, [className])}
 			onClick={toggleTheme}
 		>
-			Toggle Theme
+			{theme === Theme.DARK ? <DarkIcon /> : <LightIcon />}
 		</button>
 	);
 };
