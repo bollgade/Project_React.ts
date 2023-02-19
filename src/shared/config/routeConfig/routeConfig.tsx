024b855f530b4a68cbd95e90766426efd496c18f
BollgadeL <Bollgade@yandex.ru>
2023-02-19 16:23:40 +0300

    change routeConfig structure + Add NotFoundPage

--- src/shared/config/routeConfig/routeConfig.tsx
+++ src/shared/config/routeConfig/routeConfig.tsx
@@ -1,24 +1,28 @@
 import { AboutPage } from 'pages/AboutPage';
 import { MainPage } from 'pages/MainPage';
+import { NotFoundPage } from 'pages/NotFoundPage';
 import { RouteProps } from 'react-router-dom';
 
 export enum AppRoutes {
   MAIN = 'main',
   ABOUT = 'about',
+  NOT_FOUND = 'not_found',
 }
 
-export const RoutePath: Record<AppRoutes, string> = {
-  [AppRoutes.MAIN]: '/',
-  [AppRoutes.ABOUT]: '/about',
-};
-
-export const routeConfig: RouteProps[] = [
-  {
-    path: RoutePath.main,
+export const RoutePath: Record<AppRoutes, RouteProps> = {
+  [AppRoutes.MAIN]: {
+    path: '/',
     element: <MainPage />,
   },
-  {
-    path: RoutePath.about,
+  [AppRoutes.ABOUT]: {
+    path: '/about',
     element: <AboutPage />,
   },
-];
+  [AppRoutes.NOT_FOUND]: {
+    path: '*',
+    element: <NotFoundPage />,
+  },
+};
+
+export const routeConfig
+  : RouteProps[] = Object.values(RoutePath).map((val) => val);
