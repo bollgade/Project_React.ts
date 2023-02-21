43ec27f27556027eef59c6184adbbfe08ce4f087
bollgade <Bollgade@yandex.ru>
2023-02-21 22:18:16 +0300

    providers/ErrorBoundary: - create it; - added to main file + Created a button for generating an Error

---
+++ src/app/providers/ErrorBoundary/ui/BugButton.tsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from 'react';
+import { Button } from 'shared/ui/Button/Button';
+
+export const BugButton = () => {
+  const text = 'Throw Error';
+  const [error, setError] = useState(false);
+  const onThrow = () => setError(true);
+  useEffect(() => {
+    if (error) {
+      throw new Error();
+    }
+  }, [error]);
+
+  return (
+    <Button
+      onClick={onThrow}
+    >
+      {text}
+    </Button>
+  );
+};
---
+++ src/app/providers/ErrorBoundary/ui/ErrorBoundary.tsx
@@ -0,0 +1,45 @@
+import { PageError } from 'pages/PageError';
+import React, { ErrorInfo, ReactNode, Suspense } from 'react';
+
+interface ErrorBoundaryProps {
+  children: ReactNode;
+}
+
+interface ErrorBoundaryState {
+  hasError: boolean;
+}
+
+class ErrorBoundary
+  extends React.Component<ErrorBoundaryProps, ErrorBoundaryState> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(_error: Error) {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    // You can also log the error to an error reporting service
+    console.log(error, errorInfo); // eslint-disable-line no-console
+  }
+
+  render() {
+    const { hasError } = this.state;
+    const { children } = this.props;
+
+    if (hasError) {
+      return (
+        <Suspense fallback="">
+          <PageError />
+        </Suspense>
+      );
+    }
+
+    return children;
+  }
+}
+
+export default ErrorBoundary;
