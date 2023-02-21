40059bb852a19b868a908a26fb751f0517963f64
bollgade <Bollgade@yandex.ru>
2023-02-21 22:16:30 +0300

    create page/PageError

---
+++ src/pages/PageError/index.ts
@@ -0,0 +1,1 @@
+export { PageError } from 'pages/PageError/ui/PageError';
---
+++ src/pages/PageError/ui/PageError.tsx
@@ -0,0 +1,28 @@
+import { FC } from 'react';
+import { useTranslation } from 'react-i18next';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { Button } from 'shared/ui/Button/Button';
+import cls from './PageError.module.scss';
+
+interface PageErrorProps {
+  className?: string;
+}
+
+export const PageError: FC<PageErrorProps> = (props) => {
+  const { className } = props;
+
+  const { t } = useTranslation();
+
+  const reloadPage = () => {
+    window.location.reload();
+  };
+
+  return (
+    <div className={classNames(cls.pageError, {}, [className])}>
+      <p>{t('Error message')}</p>
+      <Button onClick={reloadPage}>
+        {t('update page')}
+      </Button>
+    </div>
+  );
+};
