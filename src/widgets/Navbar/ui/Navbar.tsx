164bcd162d204a1396a7613be3250f0e4db62c56
BollgadeL <Bollgade@yandex.ru>
2023-02-25 13:24:17 +0300

    add for eslint --max-warnings=0, and fix warnings

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,19 +1,14 @@
-import { useTranslation } from 'react-i18next';
 import { classNames } from 'shared/lib/classNames/classNames';
-import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
 import cls from './Navbar.module.scss';
 
 interface NavbarProps {
   className?: string;
 }
 
-export const Navbar = ({ className }: NavbarProps) => {
-  const { t } = useTranslation();
-  return (
-    <div className={classNames(cls.navbar, {}, [className])}>
-      <div className={classNames(cls.links, {}, [className])}>
-        /
-      </div>
+export const Navbar = ({ className }: NavbarProps) => (
+  <div className={classNames(cls.navbar, {}, [className])}>
+    <div className={classNames(cls.links, {}, [className])}>
+      /
     </div>
-  );
-};
+  </div>
+);
