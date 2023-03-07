a3109825b7d2610786338d6aa4892c145026bb8b
bollgade <Bollgade@yandex.ru>
2023-03-07 17:47:10 +0300

    shared/hooks: useAppDispatch

--- src/app/providers/StoreProvider/index.ts
+++ src/app/providers/StoreProvider/index.ts
@@ -1,3 +1,3 @@
 export type { StateSchema } from './config/StateSchema';
-export { createReduxStore } from './config/store';
+export { createReduxStore, AppDispatch } from './config/store';
 export { StoreProvider } from './ui/StoreProvider';
