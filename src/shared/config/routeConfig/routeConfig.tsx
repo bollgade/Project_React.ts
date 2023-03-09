8ad4cc61a6a52c84d8f1227aadb3eccabfc184c2
bollgade <Bollgade@yandex.ru>
2023-03-09 17:55:23 +0300

    added ProfilePage to routeConfig

--- src/shared/config/routeConfig/routeConfig.tsx
+++ src/shared/config/routeConfig/routeConfig.tsx
@@ -1,14 +1,17 @@
 import { AboutPage } from 'pages/AboutPage';
 import { MainPage } from 'pages/MainPage';
 import { NotFoundPage } from 'pages/NotFoundPage';
+import { ProfilePage } from 'pages/ProfilePage';
 import React from 'react';
 import { RouteProps } from 'react-router-dom';
 import AboutIcon from 'shared/assets/icons/about-20-20.svg';
 import MainIcon from 'shared/assets/icons/main-20-20.svg';
+import ProfileIcon from 'shared/assets/icons/profile-20-20.svg';
 
 export enum AppRoutes {
   MAIN = 'main',
   ABOUT = 'about',
+  PROFILE = 'profile',
 
   // last-one
   NOT_FOUND = 'not_found',
@@ -38,6 +41,14 @@ export const RouteElements: Record<AppRoutes, RouteItem> = {
     link: {
       Icon: AboutIcon,
       text: 'About',
+    },
+  },
+  [AppRoutes.PROFILE]: {
+    path: '/profile',
+    element: <ProfilePage />,
+    link: {
+      Icon: ProfileIcon,
+      text: 'Profile',
     },
   },
 
