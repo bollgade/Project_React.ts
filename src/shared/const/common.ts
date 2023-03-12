06074118877e33f0ef68b25707d2d74a2c3a1b48
BollgadeL <Bollgade@yandex.ru>
2023-03-12 19:50:10 +0300

    add common types

---
+++ src/shared/const/common.ts
@@ -0,0 +1,15 @@
+export enum Currency {
+  RUB = 'RUB',
+  BYN = 'BYN',
+  KZT = 'KZT',
+  AMD = 'AMD',
+  USD = 'USD',
+  EUR = 'EUR',
+}
+
+export enum Country {
+  Russia = 'Russia',
+  Belarus = 'Belarus',
+  Kazakhstan = 'Kazakhstan',
+  Armenia = 'Armenia',
+}
