8c48f3a16c1ccd2297a0bad086bdaf44be422be3
bollgade <Bollgade@yandex.ru>
2023-03-14 16:18:10 +0300

    + apiUrl as a global variable & controlled by env

--- .eslintrc.js
+++ .eslintrc.js
@@ -96,6 +96,7 @@ module.exports = {
   },
   globals: {
     __IS_DEV__: true,
+    __API__: true,
   },
   overrides: [
     {
--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -6,7 +6,7 @@ import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import { BuildOptions } from './types/config';
 
 export function buildPlugins({
-  paths, isDev, analyze,
+  paths, isDev, analyze, apiUrl,
 }: BuildOptions):
   webpack.WebpackPluginInstance[] {
   const plugins = [
@@ -20,6 +20,7 @@ export function buildPlugins({
     }),
     new webpack.DefinePlugin({
       __IS_DEV__: JSON.stringify(isDev),
+      __API__: JSON.stringify(apiUrl),
     }),
     new BundleAnalyzerPlugin({
       analyzerMode: analyze ? 'server' : 'disabled',
